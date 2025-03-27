e4d427e085e8b8d1ef9deb62f7d1566d5a398de8
sowmya-rebbavarapu <sowmyarebbavarapu@gmail.com>
2025-03-27 15:46:31 +0000

    compare

---
+++ 1_basics/comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0);
+console.log(null>=0)
+//comparisons convert null to a number treating it as 0
+
+//all the comaprisons of undefined are false
+
+//strict check === checks for the datatypes also
+console.log("2"===2)
