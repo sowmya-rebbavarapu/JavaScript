270a681429829500c78a9cca3c6e65523a3dcbfa
sowmya-rebbavarapu <sowmyarebbavarapu@gmail.com>
2025-03-27 14:46:13 +0000

    conversion

---
+++ 1_basics/conversions.js
@@ -0,0 +1,30 @@
+let score="33"
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+
+let num="33ab"
+console.log(num)
+let value=Number(num)
+console.log(typeof value)
+console.log(value)
+
+//if num if null output would be 0 and type will be object
+//if num if undefined output would be undefined and output will be NaN
+
+//"33"->33
+//"33abs"->NaN
+//true->1,false->0
+
+let isLoggedIn=1
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanisLoggedIn)
+console.log(booleanisLoggedIn)
+
+//""->false
+//"any string"->true
+
+let n=33
+let stringNumber=String(n)
+console.log(typeof stringNumber)
+console.log(stringNumber)
