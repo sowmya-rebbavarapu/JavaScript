2d81070828a90df29b37525c6d4a0dcfd3c84d1e
sowmya-rebbavarapu <sowmyarebbavarapu@gmail.com>
2025-03-27 13:58:22 +0000

    variables

---
+++ 1_basics/variables.js
@@ -0,0 +1,10 @@
+const id=12345
+let email="sowmya@gmail.com"
+var password="12345"
+city="hyderabad"
+
+//changing the variables declared with const is not allowed
+//var-issue in block scope and fuctional scope
+
+//to print all the variables at the same time
+console.table([id,email,password,city])
