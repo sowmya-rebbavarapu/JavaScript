d6976fc5e4172281ac8ae8810675ea275c7e5416
sowmya-rebbavarapu <sowmyarebbavarapu@gmail.com>
2025-03-27 14:11:29 +0000

    datatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"//treat all Js code ad newer version
+
+let name="sowmya"//string
+let age=18// integer
+let isloggedin=false //boolean
+
+//primitive data types
+//number -> 2 to pwer 53
+//BigInt
+//string=" "
+//boolean =true/false
+//null-standalone value(empty value)
+//undefined-value is not assigned
+//symbol - unique
+
+
+console.log(typeof "sowmya");
+console.log(typeof null);
+console.log(typeof undefined)
