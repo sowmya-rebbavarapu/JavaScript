42e6f37ef47d63984818cdf65d7ed3a28f67f237
sowmya-rebbavarapu <sowmyarebbavarapu@gmail.com>
2025-04-01 08:57:51 +0530

    true/false

--- 4_controlflow/2.js
+++ 4_controlflow/2.js
@@ -20,3 +20,19 @@ switch(month){
     default:
         console.log("error")
 }
+
+
+//false ,0 and -0,"",null,undefined,Nan,BigInte 0n all these are considered as false
+
+//true "0",'false'," ",[],{},function(){} -all these are true values
+
+//nullish caolescing operator (??) :null undefined
+
+const val1=null??10
+const val2=undefined??10
+console.log(val1)
+
+//terniary operator
+// condition?true:false
+const price=100
+price>=80?console.log("greater than 80") :console.log("less than 80")
